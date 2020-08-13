8511f22dc86785a57d110f0ef8d3388b6182cb67
Dakota Coppage <dakotakcoppage@me.com>
2020-08-13 05:35:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -32,4 +32,20 @@ class Shoe
     @size
   end
   
+  def material=(material)
+    @material = material
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition=(condition)
+    @condition = condition
+  end
+  
+  def condition
+    @condition
+  end
+  
 end
